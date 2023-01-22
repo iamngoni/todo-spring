8b4038d274bedc482a5b1fd0d1189e5eb2ee8c81
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-01-22 16:32:14 +0200

    feat: project setup

---
+++ src/main/java/co/modestnerd/todospring/TodoSpringApplication.java
@@ -0,0 +1,13 @@
+package co.modestnerd.todospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/modestnerd/todospring/TodoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package co.modestnerd.todospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
